badc9f2006dfe8411f098bb1c897b6f7b49ffb61
fenrirD <fenrir4504@gmail.com>
2022-08-19 14:51:56 +0900

    .env 파일 추가.

--- src/utils/customAxios.ts
+++ src/utils/customAxios.ts
@@ -9,20 +9,8 @@ interface TokenType extends AxiosRequestHeaders{
 
 const customAxios = (headers?:AxiosRequestHeaders) => {
   //TODO 추후에 header에 대한 추상화를 고려해보자. 굳이 해야 할 필요가 있을까?
-
-  // if(isAuthorization) {
-  //
-  // }
-
-  // console.log('clientApi')
-  // const headers:TokenType = {
-  //   authorization:authorization?.token
-  // }
-
-
-
   return axios.create({
-    baseURL: API_SERVER,
+    baseURL: process.env.REACT_APP_API_SERVER,
     headers: headers
   })
 }
